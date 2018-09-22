af2e217f94be461a9331c146b702ff143f84128e
Jacob Foreman <jenfarman@gmail.com>
2018-09-22 19:26:22 +0000

    Added Stripe response handler and error handling js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,28 +3,69 @@
 $(document).on('turbolinks:load', function(){
   var theForm = $('#pro_form');
   var registerBtn = $('#form-register-btn');
-  //Set Stripe public key
+//Set Stripe public key
   Stripe.setPublishablekey( $('meta:[name="stripe-public"]').attr('content') );
-  //When user clicks form submit btn
+//When user clicks form submit btn
+//prevent default submission behavior
   registerBtn.click(function(event){
     event.preventDefault();
+    submitBtn.val("Processing").prop('disabled', true);
     
-    //Collect the credit card fields
-    var ccNum = $(#card_number).val(),
-        cvcNum = $(#card_code).val(),
-        expMonth = $(#card_month).val(),
-        expYear = $(#card_year).val();
-    //Send the card info to stripe
-    Stripe.createToken({
+  //Collect the credit card fields
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+        
+  //Use Stripe JS library to check for card errors.
+    var error = false;
+    
+    //Validate card number
+    if(!Stripe.card.validateCardNumber(ccNum)) {
+      error = true;
+      alert('The credit card number appears to be invalid')
+    }
+    //Validate CVC number
+    if(!Stripe.card.validateCVC(cvcNum)) {
+      error = true;
+      alert('The CVC number appears to be invalid')
+    }
+    
+    //Validate expiration date
+    if(!Stripe.card.validateExpiry(expMonth,expYear)) {
+      error = true;
+      alert('The expiration date appears to be invalid')
+    }
+  
+    if(error){
+      //If there are card errors, don't send to stripe
+      submitBtn.prop('disabled', false).val("Sign Up");
+    } 
+      //Send the card info to stripe
+    else {
+      Stripe.createToken({
       number: ccNum,
       cvc: cvcNum,
       exp_month: expMonth,
       exp_year: expYear
     }, stripeResponseHandler);
+    }
+  
+  
+    return false;
   });
-  //prevent default submission behavior
+  
   
   //Stripe will return a card token
-  //Inject card token as hidden field into form
-  //Submit form to our Rails app
+  function stripeResponseHandler(status, response) {
+    //Get the token from the response
+    var token = response.id;
+    
+    //Inject the card token in a hidden field
+    //Inject card token as hidden field into form
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+    //Submit form to our Rails app
+    theForm.get(0).submit();
+  }
+  
 });
